04bb0af40e0f4f1a40c1466098abf10b7103c808
7amasa20 <ahmedmahmooud447@gmail.com>
2025-07-22 18:54:51 +0300

    app.js

--- app.js
+++ app.js
@@ -1,56 +1,9 @@
-const request = require('request');
-const express = require('express');
-const app = express();
+const forecast = require('./forecast')
 
-const city = process.argv[2];
-
-const geoCodeUrl = `http://api.weatherapi.com/v1/current.json?key=94b0760f63d642a2982172603250607&q=${city}&aqi=no`;
+const city = process.argv[2]
 
 
-request({ url: geoCodeUrl, json: true }, (error, response) => {
-    if (error) {
-        console.log(" 404 Not Found ")
-    }
-
-    else if (response.body.error) {
-        console.log(response.body.error.message)
-
-    }
-
-    else {
-        const { lon, lat, name } = response.body.location
-        const condition = response.body.current.condition.text
-        const temp = response.body.current.temp_c
-
-
-        console.log(`City: ${name}`);
-        console.log(`Condition: ${condition}`);
-        console.log(`Temperature: ${temp} °C`);
-        console.log(`Longitude: ${lon}, Latitude: ${lat}`);
-    }
-
-
-
-
-
-
-
-
-
-    // console.log(response.body)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+forecast(city , (error, data) => {
+    console.log('Error: ', error)
+    console.log('Data: ', data)
 })
